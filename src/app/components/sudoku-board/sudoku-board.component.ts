f5711e5655c9e0f957f30cc120ca6310a10e9a1b
Szymon Pilszek <szymon.pilszek@gmail.com>
2024-02-05 09:52:27 +0100

    initialize mvc structure

--- src/app/components/sudoku-board/sudoku-board.component.ts
+++ src/app/components/sudoku-board/sudoku-board.component.ts
@@ -1,33 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { Sudoku } from '../../models/sudoku.model';
 import { SudokuService } from '../../services/sudoku.service';
 import { SudokuCalculationService } from '../../services/sudoku-calculation.service';
 import { SudokuDTO } from '../../models/sudoku-dto.model';
 
 @Component({
-  selector: 'app-sudoku-board',
+  selector: 'sudoku-board',
   templateUrl: './sudoku-board.component.html',
   styleUrl: './sudoku-board.component.scss'
 })
 export class SudokuBoardComponent implements OnInit {
-  sudokuBoardModel: Sudoku = new Sudoku();
-  sudokuDtoModel: SudokuDTO = new SudokuDTO();
+  @Input() sudokuBoardModel: Sudoku = new Sudoku();
 
-  constructor(
-    private service: SudokuService
-    ) {}
+  constructor() {}
 
   ngOnInit(): void {
-    this.service.getBaseBoard().subscribe((res) => {
-      if (res != undefined) {
-        this.sudokuDtoModel.deserialize(res);
-        this.sudokuBoardModel.fromDTO(this.sudokuDtoModel);
-      }
-    });
-  }
-
-  getIdxFromCoordinates(row: number, col: number) {
-    return SudokuCalculationService.getIdxFromCoordinates(row, col);
   }
   
 }
--- src/app/components/sudoku/sudoku.component.ts
+++ src/app/components/sudoku/sudoku.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Sudoku } from '../../models/sudoku.model';
+import { SudokuDTO } from '../../models/sudoku-dto.model';
+import { SudokuService } from '../../services/sudoku.service';
 
 @Component({
   selector: 'app-sudoku',
@@ -6,5 +9,19 @@ import { Component } from '@angular/core';
   styleUrl: './sudoku.component.scss'
 })
 export class SudokuComponent {
+  sudokuBoardModel: Sudoku = new Sudoku();
+  sudokuDtoModel: SudokuDTO = new SudokuDTO();
 
+  constructor(
+    private service: SudokuService
+    ) {}
+
+  ngOnInit(): void {
+    this.service.getBaseBoard().subscribe((res) => {
+      if (res != undefined) {
+        this.sudokuDtoModel.deserialize(res);
+        this.sudokuBoardModel.fromDTO(this.sudokuDtoModel);
+      }
+    });
+  }
 }
