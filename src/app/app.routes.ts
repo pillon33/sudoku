6e36c2e9488e845793adbd39343360f00d9c0dd2
Szymon Pilszek <szymon.pilszek@gmail.com>
2024-02-12 11:59:03 +0100

    delete sudoku block
    delete sudoku block

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,12 +1,10 @@
 import { Routes } from '@angular/router';
 import {SudokuComponent} from './components/sudoku/sudoku.component';
 import {SudokuCellComponent} from './components/sudoku-cell/sudoku-cell.component';
-import {SudokuBlockComponent} from './components/sudoku-block/sudoku-block.component';
 import {SudokuBoardComponent} from './components/sudoku-board/sudoku-board.component';
 
 export const routes: Routes = [
     { path: 'sudoku', component: SudokuComponent },
     { path: 'sudoku-cell', component: SudokuCellComponent },
-    { path: 'sudoku-block', component: SudokuBlockComponent },
     { path: 'sudoku-board', component: SudokuBoardComponent },
 ];
