2720f9c7738e2f395995f4d28f687e63105c98a4
Szymon Pilszek <szymon.pilszek@gmail.com>
2024-02-13 11:37:22 +0100

    delete non-existing module from menu
    delete sudoku block from menu buttons

--- src/app/components/main-menu/main-menu.component.ts
+++ src/app/components/main-menu/main-menu.component.ts
@@ -9,7 +9,6 @@ export class MainMenuComponent {
   public menuElements = [
     {name: 'sudoku', menuTxt: 'Sudoku', menuDescription: 'Complete sudoku component', redirectPath: 'sudoku'},
     {name: 'sudoku-cell', menuTxt: 'Sudoku Cell', menuDescription: 'Sudoku cell component', redirectPath: 'sudoku-cell'},
-    {name: 'sudoku-block', menuTxt: 'Sudoku Block', menuDescription: 'Sudoku block component', redirectPath: 'sudoku-block'},
     {name: 'sudoku-board', menuTxt: 'Sudoku Board', menuDescription: 'Sudoku board generic component', redirectPath: 'sudoku-board'},
   ];
 
